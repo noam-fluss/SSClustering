5af7f4dc1bb91cd63a89d53f2a184341bd26ffbd
noam fluss <noam.fluss.42@gmail.com>
2022-03-22 13:49:34 +0200

    remove unnecessary

--- run_file.py
+++ run_file.py
@@ -19,50 +19,14 @@ def set_args_by_seed(args):
     return args
 
 
-def lines_that_start_with(string, fp):
-    return [line for line in fp if line.startswith(string)]
-
-
-def start_wandb():
-    chosen_path = None
-    wandb_value = int(wandb.run.name[wandb.run.name.rfind("-") + 1:])
-    folder_path = str(pathlib.Path().resolve()) + r"/scripts/"
-    file_type = r'*out'
-    files = glob.glob(folder_path + file_type)
-    files.sort(key=os.path.getctime)
-    last_files = files[-2:]
-    print("wandb_value", wandb_value)
-    for path in last_files:
-        f = open(path, "r")
-        wandb_line = lines_that_start_with("wandb_run:", f)
-        print("workers", lines_that_start_with("workers", f))
-        print("path", path)
-        print("wandb_line", wandb_line)
-        f.close()
-        print("finish!")
-        if len(wandb_line) == 0:
-            continue
-        if wandb_value == int(wandb_line[0][11:-2]):
-            chosen_path = path
-            break
-    print("chosen_path", chosen_path)
-    if chosen_path is not None:
-        os.rename(chosen_path, folder_path + "wandb_" + wandb.run.name[wandb.run.name.rfind("-") + 1:] + ".out")
-
-
 if __name__ == '__main__':
     print("start!")
 
     args = TrainingOptions().parse()
     args = set_args_by_seed(args)
     config = vars(args)
-    # TODO remove
-    if args.semi_run_num_index == 2:
-        print("this is args.semi_run_num_index")
-        exit()
     wandb.init(project="new_run_missing_labels", entity="noam-fluss", config=config)
     print("wandb_run:", wandb.run.name[wandb.run.name.rfind("-") + 1:])
 
-    #start_wandb()
     print("missing_labels", args.missing_labels)
     SSClusteringRunner(args).train()
